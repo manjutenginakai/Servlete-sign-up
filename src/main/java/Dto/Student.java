875ce7b2faf62076b0a79803708ac22b8e9dae46
unknown <manjutenginakai944@gmail.com>
2024-01-27 11:17:47 +0530

    login

--- src/main/java/Dto/Student.java
+++ src/main/java/Dto/Student.java
@@ -17,5 +17,43 @@ String email;
 long phone;
 String password;
 String Gender;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public long getPhone() {
+	return phone;
+}
+public void setPhone(long phone) {
+	this.phone = phone;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getGender() {
+	return Gender;
+}
+public void setGender(String gender) {
+	Gender = gender;
+}
+
+
 
 }
